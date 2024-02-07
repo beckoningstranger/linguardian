a2387587aeb80bb8acc5a218fbd3d5cf796d6cd3
beckoningstranger <jan.eisen@tutanota.de>
2024-02-07 15:54:15 +0100

    feat: Basic visual feedback in Translation Mode

--- components/LearningModes/TranslationMode.tsx
+++ components/LearningModes/TranslationMode.tsx
@@ -21,25 +21,33 @@ export default function TranslationMode({
   const [reviewedItems, setReviewedItems] = useState(0);
   const [activeItem, setActiveItem] = useState(items[reviewedItems]);
   const [solution, setSolution] = useState("");
+  const [inputFieldColor, setInputFieldColor] = useState("bg-slate-200");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     setSolution("");
-    console.log("Form submitted with ", solution);
-    if (activeItem.meaning[target]?.includes(solution)) {
-      console.log("Correct!");
-    } else console.log("Wrong!");
-    if (items[reviewedItems + 1]) {
-      setReviewedItems(reviewedItems + 1);
-      setActiveItem(items[reviewedItems + 1]);
-    } else {
-      console.log("Session complete");
-    }
+    const success = activeItem.meaning[target]!.includes(solution);
+
+    setInputFieldColor(success ? "bg-green-300" : "bg-red-400");
+    // This is where we can update the backend for this item, i.e. learningHistory and most of all new level and next review time
+
+    // This is where we can trigger additional reviews for this item, i.e. gender
+
+    setTimeout(() => {
+      setInputFieldColor("bg-slate-200");
+      if (items[reviewedItems + 1]) {
+        setReviewedItems(reviewedItems + 1);
+        setActiveItem(items[reviewedItems + 1]);
+      } else {
+        console.log("Session complete");
+        // This is where we navigate back to the dashboard and load fresh data from backend
+      }
+    }, 1000);
   };
 
   if (typeof activeItem.meaning !== undefined) {
     return (
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center transition-all">
         <div id="TopBar" className="bg-slate-200 text-center w-full text-xl">
           <h1 className="font-semibold">Reviewing {listName}</h1>
           <h2>
@@ -48,7 +56,7 @@ export default function TranslationMode({
         </div>
         <div
           id="Prompt"
-          className="bg-slate-200 text-center w-95 m-6 rounded-md p-3"
+          className={`bg-slate-200 text-center w-95 m-6 rounded-md p-3`}
         >
           <h3 className="my-3 text-2xl">
             {activeItem.meaning[native]?.join(", ")}
@@ -57,14 +65,14 @@ export default function TranslationMode({
         </div>
         <form
           onSubmit={handleSubmit}
-          className="w-95 mb-6 mx-6 rounded-md bg-slate-200 flex justify-stretch"
+          className={`w-95 mb-6 mx-6 rounded-md ${inputFieldColor} flex justify-stretch transition-all`}
         >
           <input
             type="text"
             placeholder={`Translate to ${convertCodeToLanguageName(target)}`}
             value={solution}
             onChange={(e) => setSolution(e.target.value)}
-            className="m-3 pt-2 text-xl bg-slate-200 text-center mx-auto w-11/12 focus:outline-none focus:border-b-2 border-b-black"
+            className={`m-3 pt-2 bg-transparent text-xl text-center mx-auto w-11/12 focus:outline-none focus:border-b-2 border-b-black`}
           />
         </form>
       </div>
