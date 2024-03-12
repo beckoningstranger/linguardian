f6c0b3343d4fed966da235eba5ac612bcf37ca17
beckoningstranger <jan.eisen@tutanota.de>
2024-03-12 16:23:06 +0100

    feat: Basic list overview page

--- client/app/lists/[listNumberString]/page.tsx
+++ client/app/lists/[listNumberString]/page.tsx
@@ -1,4 +1,5 @@
 import { getBasicListData } from "@/app/actions";
+import Image from "next/image";
 import Link from "next/link";
 
 interface ListDetailProps {
@@ -15,23 +16,69 @@ export default async function ListDetailPage({
   const listData = await getBasicListData(listNumber);
 
   if (listData && listData.data) {
-    const { name, description, authors, unitOrder } = listData.data;
-    const renderedUnits = unitOrder?.map((unit, index) => {
+    const {
+      name,
+      description = "No description entered yet...",
+      authors,
+      unitOrder,
+      units,
+    } = listData.data;
+
+    const renderedUnits = unitOrder?.map((unitName, index) => {
+      // IDEA: see how much the user has learned for each unit and calculate from- and to- values
+      // for the gradient colors to visualize their progress
       return (
-        <Link key={unit} href={`/lists/${listNumber}/${index + 1}`}>
-          {unit}
+        <Link
+          key={index}
+          href={`/lists/${listNumber}/${index + 1}`}
+          className="flex w-full justify-center"
+        >
+          <div
+            className={`${
+              index % 2 === 0 &&
+              "rounded-bl-[70px] rounded-tr-[70px] bg-gradient-to-tr"
+            } ${
+              index % 2 !== 0 &&
+              "rounded-tl-[70px] rounded-br-[70px] bg-gradient-to-tl"
+            } border border-slate-800 text-center py-6 shadow-lg hover:shadow-2xl w-10/12 flex justify-center bg-gradient-to-r from-slate-100 to-slate-100`}
+          >
+            {unitName}
+          </div>
         </Link>
       );
     });
 
+    // Calculate how many words the user already knows
+    const learned = 20;
+
     if (renderedUnits)
       return (
         <>
-          <div className="mx-10 mt-24">
-            <h1>{name}</h1>
-            <h5>by {authors}</h5>
-            <h3>{description}</h3>
-            <div>{renderedUnits}</div>
+          <div
+            id="header"
+            className="relative flex border-y-2 border-slate-300"
+          >
+            <Image
+              src="https://picsum.photos/200"
+              alt="List image"
+              height={200}
+              width={200}
+              className="w-1/3 md:w-1/6"
+              priority
+            />
+            <div className="flex w-2/3 flex-col items-center">
+              <h1 className="my-auto flex justify-center text-2xl">{name}</h1>
+              <h5 className="absolute bottom-1 right-3 text-xs">
+                created by {authors}
+              </h5>
+              <h3 className="hidden">{description}</h3>
+            </div>
+          </div>
+          <div id="progress" className="m-3 text-center text-xl">
+            {learned} / {units.length} words learned
+          </div>
+          <div id="units" className="flex flex-col items-center gap-y-2">
+            {renderedUnits}
           </div>
         </>
       );
