001523e6afba814ae56c996743ff5d48b65ae919
Jan Eisen <jan.eisen@tutanota.de>
2024-06-20 10:52:17 +0200

    feat: On "/" log user in automatically if session is still valid

--- client/app/(withoutTopMenu)/page.tsx
+++ client/app/(withoutTopMenu)/page.tsx
@@ -1,11 +1,18 @@
 import LoginForm from "@/components/authentication/LoginForm";
+import getUserOnServer from "@/lib/getUserOnServer";
+import paths from "@/paths";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
 export default async function Root() {
+  const sessionUser = await getUserOnServer();
+  if (sessionUser.native && sessionUser.isLearning)
+    redirect(paths.dashboardLanguagePath(sessionUser.isLearning[0].name));
+
   return (
     <main>
       <LoginForm />
