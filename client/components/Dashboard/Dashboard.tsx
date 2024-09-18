d0a42dbd1a14c711e2c3bc8fa0407707ef5802f4
Jan Eisen <jan.eisen@tutanota.de>
2024-09-18 15:08:35 +0200

    feat: More consistent AddNewList button on dashboard

--- client/components/Dashboard/Dashboard.tsx
+++ client/components/Dashboard/Dashboard.tsx
@@ -6,6 +6,8 @@ import getUserOnServer from "@/lib/helperFunctions";
 import paths from "@/lib/paths";
 import { SessionUser, SupportedLanguage } from "@/lib/types";
 import ListDashboardCard from "./ListDashboardCard";
+import BottomRightButton from "../BottomRightButton";
+import { renderIntoDocument } from "react-dom/test-utils";
 
 interface DashboardProps {
   language: SupportedLanguage;
@@ -52,15 +54,17 @@ export default async function Dashboard({ language }: DashboardProps) {
     }
   );
 
-  function AddNewListOption() {
+  function AddNewListOption({
+    dashboardContainsLists,
+  }: {
+    dashboardContainsLists: boolean | undefined;
+  }) {
     return (
       <Link
         href={paths.listsLanguagePath(language)}
-        className="relative mx-6 flex h-full min-h-40 items-center justify-center rounded-md bg-slate-200 md:min-h-80 lg:mx-3 xl:mx-6"
+        className={`${!dashboardContainsLists ? "animate-pulse" : ""}`}
       >
-        <div className="flex size-4/5 items-center justify-center rounded-md bg-slate-100 text-6xl text-slate-600 transition-all hover:scale-110 md:text-8xl">
-          <HiOutlinePlusCircle />
-        </div>
+        <BottomRightButton />
       </Link>
     );
   }
@@ -69,7 +73,9 @@ export default async function Dashboard({ language }: DashboardProps) {
     <div className="flex justify-center">
       <div className="grid w-full max-w-xl grid-cols-1 items-stretch justify-center gap-y-3 py-4 md:max-w-full md:grid-cols-2 lg:grid-cols-3 2xl:mx-8 2xl:max-w-[1500px] 2xl:gap-x-6">
         {renderedLists}
-        <AddNewListOption />
+        <AddNewListOption
+          dashboardContainsLists={renderedLists && renderedLists?.length > 0}
+        />
       </div>
     </div>
   );
