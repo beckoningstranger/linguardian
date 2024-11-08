cd6c2e3087138362f25159edac08036a0279cd94
Jan Eisen <jan.eisen@tutanota.de>
2024-11-08 13:23:57 +0100

    Another minor change to trigger workflow

--- client/app/(withoutTopMenu)/welcome/page.tsx
+++ client/app/(withoutTopMenu)/welcome/page.tsx
@@ -23,7 +23,7 @@ export default async function Welcome() {
     <div className="relative grid h-screen">
       <div className="mt-32 flex flex-col">
         <h1 className="absolute left-0 top-0 flex h-32 w-full items-center justify-center px-1 font-dancing text-3xl font-bold tracking-wide sm:text-6xl">
-          Welcome to Languardian!
+          Welcome to Linguardian!
         </h1>
         <Onboarding allLanguageFeatures={allLanguageFeatures} />
       </div>
