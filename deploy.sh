c08ee567253484653a1d83d9f118d3cbe3444147
Jan Eisen <jan.eisen@tutanota.de>
2024-11-12 22:47:17 +0100

    Polish deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cd ~/linguardian
-docker-compose -f docker-compose.deploy.yml down
-docker rmi 977099010577.dkr.ecr.eu-central-1.amazonaws.com/linguardian:frontend-latest 977099010577.dkr.ecr.eu-central-1.amazonaws.com/linguardian:backend-latest
+docker stop $(docker ps -q)
+docker rm $(docker ps -aq)
+docker image prune -af
 docker-compose -f docker-compose.deploy.yml up -d
