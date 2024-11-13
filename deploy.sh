7b2a168629737ec83cb90e9cda45ff38065fd969
Jan Eisen <jan.eisen@tutanota.de>
2024-11-13 13:01:46 +0100

    fix: Login to ECR again before pulling images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker stop $(docker ps -q)
 docker rm $(docker ps -aq)
 docker image prune -af
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "$AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 docker-compose -f docker-compose.deploy.yml up -d
